f2cd4c4be2c03c44d09166d5255590601f9e8be9
penyelamatbumi <inihanip7@gmail.com>
2026-04-21 17:52:46 +0700

    menamb ahkan data di int main

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -15,6 +15,13 @@ class Mahasiswa{ //blue print atau cetakan
 }; //batas class
 
 int main(){
+    Mahasiswa mhs;//object
+    mhs.nim = 2022;
+    mhs.nama = "Abra";
+    mhs.nilai = 90.5;
+
+    mhs.printData();
+}
 
 
 
