8721ada818569dbc25e198b258cae1eedd80f7b6
penyelamatbumi <inihanip7@gmail.com>
2026-04-21 17:41:48 +0700

    menambahkan namespace std

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -1,24 +1,2 @@
 #include <iostream>
 using namespace std;
-
-class Mahasiswa{ //blue print atau cetakan
-public :
-    int nim;
-    string nama;
-    float nilai;
-
-    void printData(){
-        cout << "NIM : " << nim << endl;
-        cout << "Nama : " << nama << endl;
-        cout << "Nilai : " << nilai <<endl;
-    }
-}; //batas class
-
-int main(){
-    Mahasiswa mhs;//object
-    mhs.nim = 2022;
-    mhs.nama = "Abra";
-    mhs.nilai = 90.5;
-
-    mhs.printData();
-}
